274e8827d1b4526a3fb4578598f764ee65900dee
InesRM <107609883+InesRM@users.noreply.github.com>
2023-01-24 14:48:39 +0100

    rutas para asignar tareas y avatares

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -25,22 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->prefix('prueba')->group(function () {
-
-    Route::post('eleccion/crear', [PruebaController::class,'insertPruebaEleccion'])->middleware(['midDios']);
-
-    Route::post('valoracion/crear', [PruebaController::class,'insertPruebaValoracion'])->middleware(['midDios']);
-
-    Route::post('puntual/crear', [PruebaController::class,'insertPruebaPuntual'])->middleware(['midDios']);
-
-    Route::post('resplibre/crear', [PruebaController::class,'insertPruebaRespLibre'])->middleware(['midDios']);
-
-    Route::get('listar', [PruebaController::class,'getPruebas'])->middleware(['midDios']);
-
-    Route::delete('borrar/{id}', [PruebaController::class,'deletePrueba'])->middleware(['midDios']);
-
-});
-
+//users**************
 Route::controller (UserController::class)->prefix('user')->group(function () {
     Route::put('update','updateUser')->middleware(['cors']);
     Route::put('updateattributes','updateAttributes');
@@ -50,11 +35,13 @@ Route::controller (UserController::class)->prefix('user')->group(function () {
     Route::get('delete/{id}','deleteUser')->middleware(['cors']);
 
 });
+//imagenes**************
 Route::controller(ImageUploadController::class)->prefix('image')->group(function () {
     Route::get('getAvatar/{id}','getAvatar');
     Route::post('store','store' )->middleware(['cors']);
     Route::put('updateAvatar','updateAvatar')->middleware(['cors']);
 });
+//pruebas**************
 Route::controller(PruebaController::class)->prefix('prueba')->group(function () {
     Route::post('eleccion/crear', 'insertPruebaEleccion')->middleware(['cors']);
     Route::post('valoracion/crear', 'insertPruebaValoracion')->middleware(['cors']);
@@ -63,7 +50,17 @@ Route::controller(PruebaController::class)->prefix('prueba')->group(function () 
     Route::get('listar', 'getPruebas')->middleware(['cors']);
     Route::delete('borrar/{id}', 'deletePrueba')->middleware(['cors']);
 });
+//comunes, login, logout, registro, verificacion**************
 Route::post('register', [AuthController::class, 'register'])->middleware(['cors']);
 Route::post('login', [AuthController::class, 'login'])->middleware(['cors']);
 Route::post('logout', [AuthController::class, 'logout']);
 Route::get('verification', [VerificationController::class, 'verificateMail']);
+
+//asignar**************
+Route::middleware('auth:sanctum')->prefix('asignar')->group(function () {
+    Route::get('listar/usuariosdios/{id}', [AsignarController::class,'getUsuariosDios']);
+    Route::get('listar/usuarioprotegido/{id}/{idprueba}', [AsignarController::class,'getUsuariosProtegidos']);
+    Route::get('listar/usuarioafin/{id}/{idprueba}', [AsignarController::class,'getUsuariosAfines']);
+    Route::post('asignarprueba', [AsignarController::class,'insertarUsuariosAsignados']);
+    Route::get('listar/usuariosasignados/{idprueba}', [AsignarController::class,'getUsuariosAsignados']);
+});
